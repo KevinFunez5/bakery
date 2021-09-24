fb9efa1cf2f7ef02e7e4519f32db38e475d4c082
Kevin Funez <kevin.funez315@gmail.com>
2021-09-24 14:53:53 -0700

    Add initial console application

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,6 +6,11 @@ public class Program
   static void Main()
   {
     Bread bread = new Bread();
-    
+    bread.PricePer = 5;
+    Pastry pastry = new Pastry();
+    pastry.PricePer = 2;
+
+    Console.WriteLine("All loaves of bread are "+bread.PricePer.ToString("C2")+" and all pastries are "+pastry.PricePer.ToString("C2")+".");
+    Console.WriteLine("Today we have 2 amazing specials - buy 2 loaves of bread get 1 free!!  Additionally, 3 pastries for $5!");
   }
 }
