a42e3c2e761fd678b32bf07719cdbd85b4e87dad
Kevin Funez <kevin.funez315@gmail.com>
2021-09-24 09:03:38 -0700

    Loafs methods

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -7,10 +7,23 @@ namespace Bakery.Tests
   public class BreadTests
   {
     [TestMethod]
-    public void BreadConstructor_CreatesObjectBread_Bread()
+        public void BreadConstructor_CreatesObjectBread_Bread()
     {
       Bread loaf = new Bread();
       Assert.AreEqual(typeof(Bread), loaf.GetType());
+    }
+
+    [TestMethod]
+    public void AddLoaves_AddOneLoaf_QuantityOfOrder()
+    {
+      //Arrange 
+      Bread bread = new Bread();
+
+      //Act
+      bread.AddLoaves(1);
+
+      //Assert
+      Assert.AreEqual(1, bread.QuantityOrdered);
     }
   }
 }
