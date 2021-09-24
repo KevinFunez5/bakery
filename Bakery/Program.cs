336a29fbb5c85c256ef43710cdaae30341b106a7
Kevin Funez <kevin.funez315@gmail.com>
2021-09-24 16:12:20 -0700

    adding together user entry cost

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -17,64 +17,17 @@ public class Program
     if (breadQTY != 0)
     {
       Console.WriteLine("How many pastries would you like to purchase? Enter quantity:");
-      int pastryQTY = Convert.ToInt32(Console.ReadLine());
     } 
     else 
     {
       Console.WriteLine("Thankyou!");
     }
+    int pastryQTY = Convert.ToInt32(Console.ReadLine());
     bread.AddLoaves(breadQTY);
     pastry.AddPastries(pastryQTY);
-    Console.WriteLine(breadQTY);
+
+    int Total = bread.TotalCost + pastry.TotalCost;
+    Console.WriteLine("Your total is "+Total.ToString("C2")+"");
+    // Console.WriteLine(pastryQTY);
   }
-    // string selection = "a";
-    // while(selection != "c")
-    // {
-    //   if(bread.QuantityOrdered != 0)
-    //   {
-    //     Console.WriteLine("\r\nHow many additional loaves of bread would you like to purchase?\r\nEnter quantity: ");
-    //   }
-    //   else
-    //   {
-    //     Console.WriteLine("\r\nHow many loaves of bread would you like to purchase?\r\nEnter quantity: ");
-    //   }
-    //   int breadQty = Int32.Parse(Console.ReadLine());
-    //   bread.AddLoaves(breadQty);
-    //   if(pastry.QuantityOrdered != 0)
-    //   {
-    //     Console.WriteLine("\r\nHow many additional pastries would you like to purchase?\r\nEnter quantity: ");
-    //   }
-    //   else
-    //   {
-    //     Console.WriteLine("\r\nHow many pastries would you like to purchase?\r\nEnter quantity: ");
-    //   }
-    //   int pastryQty = Int32.Parse(Console.ReadLine());
-    //   pastry.AddPastries(pastryQty);
-
-    //   int total = bread.TotalCost + pastry.TotalCost;
-    //   Console.WriteLine("\r\nYou have selected "+bread.QuantityOrdered+" loaves of bread and "+pastry.QuantityOrdered+" pastries.  Your total comes to "+total.ToString("C2"));
-    
-    //   Console.WriteLine("\r\nWould you like to add any more items?");
-    //   Console.WriteLine("a. Add more\r\nb. Start Over\r\nc. Checkout");
-    //   selection = Console.ReadLine();
-
-    //   switch (selection) 
-    //   {
-    //     case "a":
-    //       break;
-
-    //     case "b":
-    //       bread.QuantityOrdered = 0;
-    //       pastry.QuantityOrdered = 0;
-    //       Console.WriteLine("Your cart is currently empty.");
-    //       break;
-
-    //     case "c":
-    //       Console.WriteLine("\r\nThat will be "+total.ToString("C2")+". Thank you have a great day!\r\n");
-    //       break;
-
-    //     default:
-    //       break;
-    //   }
-    // }
 }
